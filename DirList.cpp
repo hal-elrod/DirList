76bb5d2507c14d5d228eda9a31b995dda4741f76
Hal Elrod <helrod@gmail.com>
2023-09-19 12:49:47 -0500

    cleanup file

--- DirList.cpp
+++ DirList.cpp
@@ -2,35 +2,50 @@
 #include <iostream>
 #include <filesystem>
 #include <fstream>
+
 namespace fs = std::filesystem;
 
 template <typename TP>
 std::time_t to_time_t(TP tp)
 {
-    using namespace std::chrono;
-    auto sctp = time_point_cast<system_clock::duration>(tp - TP::clock::now()
-        + system_clock::now());
-    return system_clock::to_time_t(sctp);
+	using namespace std::chrono;
+	auto sctp = time_point_cast<system_clock::duration>(tp - TP::clock::now()
+		+ system_clock::now());
+	return system_clock::to_time_t(sctp);
 }
 
-int main(int argc, char **argv[])
+
+int main(int argc, char** argv[])
 {
-    if (argc < 3)
-    {
-        std::cout << "dirlist path outfile" << std::endl;
-        return(0);
-    }
+	if (argc < 3)
+	{
+		std::cout << "dirlist path outfile" << std::endl;
+		return(0);
+	}
 
-    std::cout << "from " << (char *) argv[1] << " to " << (char *) argv[2] << std::endl;
-    std::ofstream myfile;
-    myfile.open((char*)argv[2]);
+	std::cout << "from " << argv[1] << " to " << argv[2] << std::endl;
+	std::ofstream myfile;
 
-    std::string path = (char *) argv[1];
-    for (const auto& entry : fs::directory_iterator(path))
-    {
-        std::time_t tt = to_time_t(entry.last_write_time());
-        std::tm* gmt = std::gmtime(&tt);
-        myfile << entry.path() << "," << entry.file_size() << "," <<
-            std::put_time(gmt, "%m/%d/%Y") << std::endl;
-    }
+	try {
+		myfile.open((char*)argv[2]);
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Error opening file " << argv[2] << " Error " << e.what() << std::endl;
+		return(0);
+	}
+
+	std::string path = (char*)argv[1];
+	for (const auto& entry : fs::directory_iterator(path))
+	{
+		std::time_t tt = to_time_t(entry.last_write_time());
+
+		std::tm gmt;
+		gmtime_s(&gmt, &tt); // convert to GMT
+		myfile << entry.path() << "," << entry.file_size() << "," <<
+			std::put_time(&gmt, "%m/%d/%Y %H:%M:%S") << std::endl;
+	}
+	myfile.close();
+	return(EXIT_SUCCESS);
+
 }
